f64026fe3414860e861a4178292d0bd55131a446
priyanka <pp246@njit.edu>
2022-11-20 00:58:06 -0500

    Initial Commit

---
+++ lib/wordguesser_game.rb
@@ -0,0 +1,72 @@
+class WordGuesserGame
+
+  # add the necessary class methods, attributes, etc. here
+  # to make the tests in spec/wordguesser_game_spec.rb pass.
+  attr_accessor :word, :guesses, :wrong_guesses
+
+  # Get a word from remote "random word" service
+
+  def initialize(word)
+    @word = word
+    @guesses = ""
+    @wrong_guesses = ""
+  end
+
+  # You can test it by installing irb via $ gem install irb
+  # and then running $ irb -I. -r app.rb
+  # And then in the irb: irb(main):001:0> WordGuesserGame.get_random_word
+  #  => "cooking"   <-- some random word
+  def self.get_random_word
+    require 'uri'
+    require 'net/http'
+    uri = URI('http://randomword.saasbook.info/RandomWord')
+    Net::HTTP.new('randomword.saasbook.info').start { |http|
+      return http.post(uri, "").body
+    }
+  end
+
+  def new(word)
+    @hangpersonGame = HangpersonGame.new(word)
+  end
+
+  def guess(char)
+    if char =~ /[[:alpha:]]/
+      char.downcase!
+      if @word.include? char and !@guesses.include? char
+        @guesses.concat char
+        return true
+      elsif !@wrong_guesses.include? char and !@word.include? char
+        @wrong_guesses.concat char
+        return true
+      else
+        return false
+      end
+    else
+      char = :invalid
+      raise ArgumentError
+    end
+  end
+
+  def word_with_guesses
+    result = ""
+    @word.each_char do |letter|
+      if @guesses.include? letter
+        result.concat letter
+      else
+        result.concat '-'
+      end
+    end
+    return result
+  end
+
+  def check_win_or_lose
+    counter = 0
+    return :lose if @wrong_guesses.length >= 7
+    @word.each_char do |letter|
+      counter += 1 if @guesses.include? letter # if letter =~ /[#{@guesses}]/
+    end
+    if counter == @word.length then :win
+    else :play end
+  end
+
+end
