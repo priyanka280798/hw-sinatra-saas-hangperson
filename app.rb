f953fab6f0dd4ae0e6974199a3e97fdfc70b798f
priyanka280798 <98357777+priyanka280798@users.noreply.github.com>
2022-11-20 01:14:08 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -15,8 +15,7 @@ class WordGuesserApp < Sinatra::Base
     session[:game] = @game
   end
 
-  # These two routes are good examples of Sinatra syntax
-  # to help you with the rest of the assignment
+
   get '/' do
     redirect '/new'
   end
@@ -26,17 +25,14 @@ class WordGuesserApp < Sinatra::Base
   end
 
   post '/create' do
-    # NOTE: don't change next line - it's needed by autograder!
+   
     word = params[:word] || WordGuesserGame.get_random_word
-    # NOTE: don't change previous line - it's needed by autograder!
-
+   
     @game = WordGuesserGame.new(word)
     redirect '/show'
   end
 
-  # Use existing methods in WordGuesserGame to process a guess.
-  # If a guess is repeated, set flash[:message] to "You have already used that letter."
-  # If a guess is invalid, set flash[:message] to "Invalid guess."
+ 
   post '/guess' do
     if params[:guess].to_s[0] =~ /[[:alpha:]]/
       letter = params[:guess].to_s[0]
@@ -51,24 +47,20 @@ class WordGuesserApp < Sinatra::Base
     redirect '/show'
   end
 
-  # Everytime a guess is made, we should eventually end up at this route.
-  # Use existing methods in WordGuesserGame to check if player has
-  # won, lost, or neither, and take the appropriate action.
-  # Notice that the show.erb template expects to use the instance variables
-  # wrong_guesses and word_with_guesses from @game.
+ 
   get '/show' do
     if @game.check_win_or_lose == :win then redirect '/win'
     elsif @game.check_win_or_lose == :lose then redirect '/lose'
-    else erb :show end # You may change/remove this line
+    else erb :show end 
   end
 
   get '/win' do
-    if @game.check_win_or_lose == :win then erb :win # You may change/remove this line
+    if @game.check_win_or_lose == :win then erb :win 
     else redirect '/show' end
   end
 
   get '/lose' do
-    if @game.check_win_or_lose == :lose then erb :lose # You may change/remove this line
+    if @game.check_win_or_lose == :lose then erb :lose 
     else redirect '/show' end
   end
 
